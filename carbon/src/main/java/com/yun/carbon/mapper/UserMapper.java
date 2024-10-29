10e7f0e7e02333a959232a027dd7e295f34097a7
keeper-yun <154521005+keeper-yun@users.noreply.github.com>
2024-10-29 18:52:03 +0800

    Delete carbon directory

--- carbon/src/main/java/com/yun/carbon/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.yun.carbon.mapper;
-
-import com.yun.carbon.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserMapper extends JpaRepository<User,Integer> {
-    User findByUsername(String username);
-}
